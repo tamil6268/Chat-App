5940a478c8467b100c86b6bb800e294e9b01b05d
TAMILARASAN S <ts7371797@gmail.com>
2023-03-08 13:01:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const socket=require('socket.io');
+const path=require('path');
+const app=express();
+app.use(express.static(path.join(__dirname,'/public')));
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname,'/public/index.html');
+})
+const server=app.listen(8080,()=>{
+    console.log("Server Started at http://localhost:8080");
+})
+
+const io=socket(server,{cors:{origin:"*"}});
+const users={};
+let Name;
+io.on('connection',(socket)=>{
+    socket.on('user-joined',(username)=>{
+        users[socket.id]=username;
+        socket.broadcast.emit('user-joined-in',username);
+        Name=username;
+    });
+
+    socket.on('send',(message)=>{
+        socket.broadcast.emit('reached',{message:message,name:users[socket.id]});
+     })
+
+    socket.on('disconnect',()=>{
+        socket.broadcast.emit('left',users[socket.id])
+        delete users[socket.id];
+    })
+})
