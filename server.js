a4a66c822169bc1a7976c22bc3ece8c15b0f5a53
TAMILARASAN S <111070542+tamil6268@users.noreply.github.com>
2023-03-14 17:00:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static(path.join(__dirname,'/public')));
 app.get('/',(req,res)=>{
     res.sendFile(__dirname,'/public/index.html');
 })
-const server=app.listen(https://heisenberg-tamil-chat-app.onrender.com,()=>{
+const server=app.listen(8080,()=>{
     console.log("Server Started at http://localhost:8080");
 })
 
